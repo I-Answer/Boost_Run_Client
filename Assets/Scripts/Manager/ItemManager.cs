83227041f2e20986b5d5daecade6b043e759c348
AllOne1129 <allone1129@gmail.com>
2018-12-12 16:57:01 +0900

    Update Item

--- Assets/Scripts/Manager/ItemManager.cs
+++ Assets/Scripts/Manager/ItemManager.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 
 public class ItemManager : RandomPooler {
 
+    public GameObject Player;
+    private Player playerInfo;
     public float appearProbability;
 
     protected override void OnActivate() {
         if (Random.Range(0f, 1f) > appearProbability) return;
 
+        playerInfo = Player.GetComponent<Player>();
         Request();
+    }
+
+    public void IncreaseSpeed()
+    {
+        playerInfo.
     }
 }
