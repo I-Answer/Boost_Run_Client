43a18e1c6069151dad09e69b7203e82928f0ebab
AllOne1129 <allone1129@gmail.com>
2018-12-12 20:28:19 +0900

    Add particle

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,6 +1,12 @@
-﻿public class Obstacle : FieldObject {
+﻿using UnityEngine;
+
+public class Obstacle : FieldObject {
+
+    public ParticleSystem particle;
 
     protected override void OnCollision() {
+        Destroy(gameObject);
+        particle.Play();
         Player.Collision();
     }
 }
