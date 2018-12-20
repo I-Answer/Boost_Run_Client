047baa014b01194a4f87535fb6c8c4d189cebbf2
blAs1N <hwiasin210@naver.com>
2018-12-20 20:30:28 +0900

    Change stage scene background

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour {
 
     private static SoundManager instance;
     private AudioSource effectAudio, backgroundAudio;
+
+    public AudioClip homeBackgroundAudio, stageBackgroundAudio;
 
     public void Awake() {
         if (instance != null) return;
@@ -20,11 +23,31 @@ public class SoundManager : MonoBehaviour {
         }
     }
 
+    private void ChangeBackgroundSound(Scene scene, LoadSceneMode sceneMode) {
+        switch ((SceneState)scene.buildIndex) {
+            case SceneState.HOME:
+                backgroundAudio.clip = homeBackgroundAudio;
+                break;
+
+            case SceneState.STAGE:
+                backgroundAudio.clip = stageBackgroundAudio;
+                break;
+
+            case SceneState.LOADING:
+                backgroundAudio.Stop();
+                return;
+        }
+
+        backgroundAudio.Play();
+    }
+
     public static void Init(string name) {
         instance.InitImpl(name);
     }
 
     private void InitImpl(string name) {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += ChangeBackgroundSound;
+
         effectAudio.mute = Convert.ToBoolean(PlayerPrefs.GetString(string.Format("{0} Effect Sound", name), "false"));
         backgroundAudio.mute = Convert.ToBoolean(PlayerPrefs.GetString(string.Format("{0} Background Sound", name), "false"));
 
